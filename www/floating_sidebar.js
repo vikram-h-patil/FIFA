356287e4ea18a3c101870f29ae7b4f4a16a03aee
Vikram <41810540+vikram-patil1289@users.noreply.github.com>
2019-06-24 18:04:47 +1000

    Add files via upload

---
+++ www/floating_sidebar.js
@@ -0,0 +1,20 @@
+$(function() {
+
+    var $sidebar   = $(".well"),
+        $window    = $(window),
+        offset     = $sidebar.offset(),
+        topPadding = -30;
+
+    $window.scroll(function() {
+        if ($window.scrollTop() > offset.top + 70) {
+            $sidebar.stop().animate({
+                marginTop: $window.scrollTop() - offset.top + topPadding
+            });
+        } else {
+            $sidebar.stop().animate({
+                marginTop: 0
+            });
+        }
+    });
+
+});
